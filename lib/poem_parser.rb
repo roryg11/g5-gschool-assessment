fdc5db0fa2a29ea777d18f2c4be7d7529dec7f87
roryg11 <Rory.C.Grant@gmail.com>
2014-12-18 12:00:35 -0800

    some commented out methods, pseudo code etc-- FINAL PUSH

--- lib/poem_parser.rb
+++ lib/poem_parser.rb
@@ -1,61 +1,16 @@
 
+#Process:
+#tried regex to figure everything out and realized that
+#I needed to put my information in using the "lines" method
+#from there I was able to get title and author
+#struggled with finding a way to get verses and lines, but tried using regex
+#regex didn't solve my line/verse problems
+#wrote out potential code to pull in all of the poem files and push them into a hash
+# will find below
+# tried putting these in a class, but I was running out of time, so I opted to
+# just make general methods... although I can't get some of them to work.
+# would love to finish this over break.
 
-#need a program to read through text files and -- File.read
-#store information based on the /
-#text until / = title
-# text at 2nd / = Author
-# then start counting lines, exluding lines that have / without any text
-# verses are in between solitary /
-# Additionally each line starts with a capital letter
-# cannot figure out how ruby reads texts files-- REGEX!!!!
-#struggling now to figure out the right code to use with REGEX
-# using Rubular.com to find out the proper regex to extract the title
-
-#should we put this in a class--
-
-class Poem
-  attr_reader :poem
-
-  def initialize(poem)
-    @poem = poem
-  end
-
-  def poem_to_array
-    @poem.lines
-  end
-
-  def title
-    poem_array = @poem.lines
-    title = poem_array[0].chomp
-  end
-
-  def author
-    poem_array = @poem.lines
-    author = poem_array[1].chomp
-  end
-
-  #method that gives you the number of verses in the poem
-  def number_of_verses
-    verse_array = []
-    @poem.each_line do |line|
-      if line =~ /\n/ && line !~ /a-zA-Z/
-        verse_array.push("verse")
-      end
-    end
-    verse_array.length
-  end
-
-  #method that identifies the number of lines in the poem
-  def number_of_lines
-    line_array = []
-    @poem.each_line do |line|
-      line_array << line.count
-    end
-    line_array.length
-  end
-end
-
-#non-class methods
 
 poem = File.read('data/poem_01.txt')
 
@@ -72,6 +27,8 @@ def author(poem)
   poem_array = poem.lines
   author = poem_array[1].chomp
 end
+
+#feel free to comment these out
 
 def number_of_verses(poem)
   verse_array = []
@@ -91,9 +48,10 @@ def number_of_lines(poem)
   line_array.length
 end
 
-#do this for poem objects?
+# Iterating through all of the files and putting it into a hash, didn't get far
+# enough to get this to work
 
-Dir.glob('data/*.rb').each do |poem|
-  poems["#{author(poem)}"] = "#{title(poem)}"
-  poems["#{author(poem)}"]["#{title(poem)}"] = {verse: number_of_verses(poem), lines: number_of_lines(poem)}
-end
+# Dir.glob('data/*.rb').each do |poem|
+#   poems["#{author(poem)}"] = "#{title(poem)}"
+#   poems["#{author(poem)}"]["#{title(poem)}"] = {verse: number_of_verses(poem), lines: number_of_lines(poem)}
+# end

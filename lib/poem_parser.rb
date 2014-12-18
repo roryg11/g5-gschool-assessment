22ffe480b7fb15276160a7521dfeb9c35c0315f3
roryg11 <Rory.C.Grant@gmail.com>
2014-12-18 10:48:30 -0800

    initial commit... sort of. SORRY

---
+++ lib/poem_parser.rb
@@ -0,0 +1,35 @@
+
+
+#need a program to read through text files and -- File.read
+#store information based on the /
+#text until / = title
+# text at 2nd / = Author
+# then start counting lines, exluding lines that have / without any text
+# verses are in between solitary /
+# Additionally each line starts with a capital letter
+# cannot figure out how ruby reads texts files
+
+
+poem = File.read('data/poem_01.txt')
+
+poem.
+
+#method that gives you the poem title
+def title(poem)
+  #first line of text file before \n
+end
+
+#method that gives you the poem author
+def author(poem)
+end
+
+#method that gives you the number of verses in the poem
+def number_of_verses(poem)
+end
+
+#method that identifies the number of lines in the poem
+def number_of_lines(poem)
+end
+
+Dir.glob('data/*.rb').each do |f|
+end

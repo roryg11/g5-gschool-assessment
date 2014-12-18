eab9d11c09a1c81cf5f9466d79c4409e5b40a70b
roryg11 <Rory.C.Grant@gmail.com>
2014-12-18 11:11:44 -0800

    adding the method that makes the poem hash

--- lib/poem_parser.rb
+++ lib/poem_parser.rb
@@ -7,29 +7,38 @@
 # then start counting lines, exluding lines that have / without any text
 # verses are in between solitary /
 # Additionally each line starts with a capital letter
-# cannot figure out how ruby reads texts files
+# cannot figure out how ruby reads texts files-- REGEX!!!!
+#struggling now to figure out the right code to use with REGEX
+
 
 
 poem = File.read('data/poem_01.txt')
 
-poem.
-
 #method that gives you the poem title
 def title(poem)
-  #first line of text file before \n
+  #poem =~ /from start of the line to the first return/
+  #should only return that string
 end
 
 #method that gives you the poem author
 def author(poem)
+  #poem =~ /from the start of the 2nd line to the 2nd return/
+  #should only return this string
 end
 
 #method that gives you the number of verses in the poem
 def number_of_verses(poem)
+  #poem
+  # should return an integer
 end
 
 #method that identifies the number of lines in the poem
 def number_of_lines(poem)
 end
 
-Dir.glob('data/*.rb').each do |f|
+poems = {}
+
+Dir.glob('data/*.rb').each do |poem|
+  poems["#{poem.author}"] = "#{poem.title}"
+  poems["#{poem.author}"]["#{poem.title}"] = {verse: poem.number_of_verses, lines: poem.number_of_lines}  
 end
